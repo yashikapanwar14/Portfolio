3730dac8732724a57327b9581b0e784a5daf9af1
Yashika Panwar <144528291+yashikapanwar14@users.noreply.github.com>
2024-07-25 22:34:02 +0530

    Add files via upload

---
+++ yashika-portfolio/app.js
@@ -0,0 +1,14 @@
+let phone = document.querySelector(".phone");
+// let navAbout = document.querySelector(".nav-about");
+// let about = document.querySelector(".about");
+
+phone.onclick = () => {
+    phone.innerText = "8168611151";
+}
+
+// navAbout.onclick =() => {
+//     let about = addEventListener(about,callback)
+// }
+
+
+// navAbout.addEventListener("click", about);
